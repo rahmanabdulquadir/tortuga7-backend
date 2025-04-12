3dccc1409faf2d00cdf514b731a0955e2c371488
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-04-12 14:27:44 +0600

    blog bugs fixed

--- src/main/blog/create-blog.dto.ts
+++ src/main/blog/create-blog.dto.ts
@@ -15,9 +15,9 @@ export class CreateBlogDto {
   @IsString()
   image?: string;
 
-  @ApiProperty()
-  @IsString()
-  author: string;
+//   @ApiProperty()
+//   @IsString()
+//   author: string;
 
   @ApiProperty()
   @IsString()
